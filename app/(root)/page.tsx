6de0dbfeb71dfcb2744e903127d2a8238ea8516d
Sumpah Gw Bukan Lolicon <51400321+xyelea@users.noreply.github.com>
2024-02-18 19:03:22 +0700

    update : layout sidebar & mobile nav

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,7 @@
 import { UserButton } from "@clerk/nextjs";
 
 function Home() {
-  return (
-    <div>
-      <p>Home</p>
-
-      <UserButton afterSignOutUrl="/" />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default Home;
